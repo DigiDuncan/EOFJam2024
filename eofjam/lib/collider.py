43e8ddf75a5aa0b5e608761866d28d383422c112
DragonMoffon <happydog.aja@gmail.com>
2025-01-18 23:38:40 +1300

    Collider Test

--- eofjam/lib/collider.py
+++ eofjam/lib/collider.py
@@ -1,13 +1,23 @@
 from __future__ import annotations
 from typing import Protocol
+from enum import Enum, auto
 
 from arcade import Rect, Vec2, draw_rect_outline, draw_circle_outline
 from arcade.color import RED, GREEN
 import arcade
 
 
+class ColliderShape(Enum):
+    CIRCLE = auto()
+    CIRCLE_INVERTED = auto()
+    RECT = auto()
+    RECT_INVERTED = auto()
+    POLYGON = auto() # Not Implimented
+    POLYGON_INVERTED = auto() # Not Implimented
+
 class Collider(Protocol):
     position: Vec2
+    shape: ColliderShape
 
     def contains(self, point: Vec2) -> bool:
         ...
@@ -32,9 +42,10 @@ class CircleCollider(Collider):
     def __init__(self, position: Vec2, radius: float):
         self.position = position
         self.radius = radius
+        self.shape = ColliderShape.CIRCLE
 
-    def contains(self, point):
-        return (self.position - point).length_squared <= self.radius**2
+    def contains(self, point: Vec2):
+        return (self.position - point).length_squared() <= self.radius**2
 
     def overlaps(self, other: Collider) -> bool:
         return other.distance(self.position) - self.radius <= 0.0
@@ -53,7 +64,21 @@ class CircleCollider(Collider):
 
     def draw(self):
         draw_circle_outline(self.position.x, self.position.y, self.radius, RED)
+        
+class InverseCircleCollider(Collider):
+    
+    def __init__(self, position: Vec2, radius: float):
+        self.position = position
+        self.radius = radius
+        self.shape = ColliderShape.CIRCLE_INVERTED
 
+    def contains(self, point: Vec2):
+        return (self.position - point).length_squared() >= self.radius**2
+    
+    def draw(self):
+        draw_circle_outline(self.position.x, self.position.y, self.radius, GREEN)
+
+    # TODO
 
 class RectCollider(Collider):
 
@@ -67,15 +92,18 @@ class RectCollider(Collider):
     def position(self):
         return self.rect.center
 
+    @position.setter
+    def position(self, position: Vec2):
+        self.rect = self.rect.align_center(position)
+
     def overlaps(self, other: Collider):
         diff = (self.position - other.position).length()
         return self.distance(other.position) + other.distance(self.position) - diff <= 0.0
-    
+
     def collide(self, other: Collider) -> tuple[float, Vec2]:
         normal = other.direction(self.position)
         diff = (self.position - other.position).length()
         return self.distance(other.position) + other.direction(self.position) - diff, normal
-
 
     def distance(self, point):
         x, y, w, h = self.rect.xywh
@@ -107,7 +135,7 @@ class RectCollider(Collider):
             return Vec2(-1.0, 0.0)
         else:
             return Vec2(0.0, 0.0)
-    
+
     def draw(self):
         draw_rect_outline(self.rect, arcade.color.RED)
 
@@ -121,13 +149,17 @@ class InverseRectCollider(Collider):
     def position(self):
         return self.rect.center
     
+    @position.setter
+    def position(self, position: Vec2):
+        self.rect = self.rect.align_center(position)
+
     def contains(self, point):
         return not self.rect.point_in_rect(point)
 
     def overlaps(self, other: Collider):
         diff = (self.position - other.position).length()
         return self.distance(other.position) + other.distance(self.position) - diff <= 0.0
-    
+
     def collide(self, other: Collider) -> tuple[float, Vec2]:
         normal = other.direction(self.position)
         diff = (self.position - other.position).length()
@@ -163,6 +195,106 @@ class InverseRectCollider(Collider):
             return Vec2(1.0, 0.0)
         else:
             return Vec2(0.0, 0.0)
-    
+
     def draw(self):
         draw_rect_outline(self.rect, GREEN)
+
+# collision functions return the collision normal and the intersection depth
+# norm is experienced by a (b -> a)
+# The depth is eqivalent for both
+
+def collide_circles(a: CircleCollider, b: CircleCollider) -> tuple[Vec2, float]:
+    diff = a.position - b.position
+    norm = diff.normalize()
+    return norm, diff.length() - a.radius - b.radius
+
+def collide_circle_inverse(a: CircleCollider, b: InverseCircleCollider) -> tuple[Vec2, float]:
+    diff = b.position - a.position
+    norm = diff.normalize()
+    return norm, b.radius - diff.length() - a.radius
+
+def collide_circle_rect(a: CircleCollider, b: RectCollider) -> tuple[Vec2, float]:
+    pass
+
+def collide_circle_rect_inverse(a: CircleCollider, b: RectCollider) -> tuple[Vec2, float]:
+    pass
+
+def collide_circles_inverted(a: InverseCircleCollider, b: CircleCollider) -> tuple[Vec2, float]:
+    # There is no way for these to not intersect lmao
+    diff = b.position - a.position
+    norm = diff.normalize()
+    return -norm, -abs(diff.length() - a.radius - b.radius)
+
+def collide_circle_inverse_rect(a: InverseCircleCollider, b: RectCollider) -> tuple[Vec2, float]:
+    pass
+
+def collide_circle_rect_inverted(a: InverseCircleCollider, b: InverseRectCollider) -> tuple[Vec2, float]:
+    pass
+
+def collide_rects(a: RectCollider, b: RectCollider) -> tuple[Vec2, float]:
+    tb = b.rect.bottom - a.rect.top
+    bt = b.rect.top - a.rect.bottom
+    lr = b.rect.right - a.rect.left
+    rl = b.rect.left - a.rect.right
+
+    edge = max(tb, bt, lr, rl)
+
+    if edge == tb: # top collision
+        return Vec2(0.0, 1.0), tb
+    elif edge == bt: # bottom collision
+        return Vec2(0.0, -1.0), bt
+    elif edge == lr: # left collision
+        return Vec2(1.0, 0.0), lr
+    elif edge == rl: # right collision
+        return Vec2(-1.0, 0.0), rl
+
+def collide_rect_inverse(a: RectCollider, b: InverseRectCollider) -> tuple[Vec2, float]:
+    pass
+
+def collide_rects_inverted(a: InverseRectCollider, b: InverseRectCollider) -> tuple[Vec2, float]:
+    pass
+
+
+def collide(a: Collider, b: Collider) -> tuple[Vec2, float]:
+    # I see why maybe this isn't the best way to do this -.-
+    match (a.shape, b.shape):
+        case (ColliderShape.CIRCLE, ColliderShape.CIRCLE):
+            return collide_circles(a, b)
+        case (ColliderShape.CIRCLE, ColliderShape.CIRCLE_INVERTED):
+            return collide_circle_inverse(a, b)
+        case (ColliderShape.CIRCLE, ColliderShape.RECT):
+            return collide_circle_rect(a, b)
+        case (ColliderShape.CIRCLE, ColliderShape.RECT_INVERTED):
+            return collide_circle_rect_inverse(a, b)
+        case (ColliderShape.CIRCLE_INVERTED, ColliderShape.CIRCLE):
+            n, d = collide_circle_inverse(b, a)
+            return -n, d
+        case (ColliderShape.CIRCLE_INVERTED, ColliderShape.CIRCLE_INVERTED):
+            return collide_circles_inverted(a, b)
+        case (ColliderShape.CIRCLE_INVERTED, ColliderShape.RECT):
+            return collide_circle_inverse_rect(a, b)
+        case (ColliderShape.CIRCLE_INVERTED, ColliderShape.RECT_INVERTED):
+            return collide_circle_rect_inverted(a, b)
+        case (ColliderShape.RECT, ColliderShape.CIRCLE):
+            n, d = collide_circle_rect(b, a)
+            return -n, d
+        case (ColliderShape.RECT, ColliderShape.CIRCLE_INVERTED):
+            n, d = collide_circle_inverse_rect(b, a)
+            return -n, d
+        case (ColliderShape.RECT, ColliderShape.RECT):
+            return collide_rects(a, b)
+        case (ColliderShape.RECT, ColliderShape.RECT_INVERTED):
+            return collide_rect_inverse(a, b)
+        case (ColliderShape.RECT_INVERTED, ColliderShape.CIRCLE):
+            n, d = collide_circle_inverse_rect(b, a)
+            return -n, d
+        case (ColliderShape.RECT_INVERTED, ColliderShape.CIRCLE_INVERTED):
+            n, d = collide_circle_rect_inverted(b, a)
+            return -n, d
+        case (ColliderShape.RECT_INVERTED, ColliderShape.RECT):
+            n, d = collide_rect_inverse(b, a)
+            return -n, d
+        case (ColliderShape.RECT_INVERTED, ColliderShape.RECT_INVERTED):
+            return collide_rects_inverted(a, b)
+        case _:
+            raise ValueError(f'{a} or {b} are of unknown/unimplemented shapes')
--- eofjam/views/collision_test.py
+++ eofjam/views/collision_test.py
@@ -1,67 +1,146 @@
+import arcade.clock
 from eofjam.core.application import View
-from eofjam.lib.collider import RectCollider, CircleCollider, InverseRectCollider, Collider
+from eofjam.lib.collider import CircleCollider, InverseCircleCollider, collide, Collider
 
 import arcade
+
+GRAVITY = 981
+BIAS = 0.05
+SLOP = 2
+DRAG = 0.0005
+
+class Body:
+
+    def __init__(self, shape: Collider, mass: float, velocity: arcade.Vec2):
+        self.shape = shape
+        self.mass = mass
+        self.inv = 0.0 if mass == 0.0 else 1.0 / mass
+        self.velocity = velocity
+
+    def __hash__(self):
+        return hash(id(self))
+    
+    def __eq__(self, value):
+        return id(self) == id(value)
+    
+class Collision:
+
+    def __init__(self, a: Body, b: Body):
+        self.a = a
+        self.b = b
+
+        self.impulse: float = 0
+        self.normal: arcade.Vec2 = arcade.Vec2()
+
+        self.persistent = False
+
+    def compute_impulse(self):
+        n, d = collide(self.a.shape, self.b.shape)
+        effective_mass = 0.0 if (self.a.inv + self.b.inv) == 0.0 else 1.0 / (self.a.inv + self.b.inv)
+        self.normal = n
+        return effective_mass * (n.dot(self.b.velocity - self.a.velocity) - BIAS * (d - SLOP) / arcade.clock.GLOBAL_CLOCK.delta_time)
+
+    def apply_impulse(self, impulse: arcade.Vec2):
+        self.a.velocity = self.a.velocity + self.a.inv * impulse
+        self.b.velocity = self.b.velocity - self.b.inv * impulse
 
 class CollisionView(View):
 
     def __init__(self) -> None:
         super().__init__()
-        self.rect_collider = RectCollider(arcade.LBWH(300, 200, 100, 300))
-        self.circle_collider = CircleCollider(arcade.Vec2(800, 300), 200)
-        self.inverse_collider = InverseRectCollider(self.window.rect.scale(0.8))
+        self.bounds = Body(InverseCircleCollider(arcade.Vec2(self.center_x, self.center_y), self.height / 2.0 - 20), 0.0, arcade.Vec2())
+        self.bodies = [
+            self.bounds,
+            Body(CircleCollider(arcade.Vec2(self.center_x - 100, self.center_y), 30), 30.0, arcade.Vec2(300, 0)),
+            Body(CircleCollider(arcade.Vec2(self.center_x + 100, self.center_y), 30), 30.0, arcade.Vec2(-300, 0))
+        ]
+        self.collisions: dict[tuple[Body, Body], Collision] = {}
 
-        self.mouse: Collider = None
-        self.mouse_size = 400
-        self.mouse_t = 1
+        self.iterations = 10
 
-    def _make_mouse(self, pos, size):
-        self.mouse = None
-        match self.mouse_t:
-            case 1:
-                self.mouse = RectCollider(arcade.XYWH(pos[0], pos[1], size, size))
-            case 2:
-                self.mouse = CircleCollider(arcade.Vec2(*pos), size)
-            case 3:
-                self.mouse = InverseRectCollider(arcade.XYWH(pos[0], pos[1], size, size))
+        self.grabbed = None
+        self.g_mass = 0.0
 
     def on_draw(self) -> None:
         self.clear()
-        self.rect_collider.draw()
-        self.circle_collider.draw()
-        self.inverse_collider.draw()
+        for body in self.bodies:
+            body.shape.draw()
 
-        if self.mouse is None:
+    def collision(self, a: Body, b: Body):
+        n, d = collide(a.shape, b.shape)
+
+        if 0 < d:
+            if (a, b) in self.collisions:
+                self.collisions.pop((a, b))
+            elif (b, a) in self.collisions:
+                self.collisions.pop((b, a))
             return
         
-        self.mouse.draw()
+        if (a, b) in self.collisions:
+            collision = self.collisions[(a, b)]
+            collision.persistent = True
+        elif (b, a) in self.collisions:
+            collision = self.collisions[(b, a)]
+            collision.persistent = True
+        else:
+            collision = Collision(a, b)
+            self.collisions[(a, b)] = collision
 
-        if self.rect_collider.overlaps(self.mouse):
-            arcade.draw_text('collides!', self.rect_collider.position.x, self.rect_collider.position.y, arcade.color.WHITE, anchor_x='center')
+        old_impulse = collision.impulse
+        delta = collision.compute_impulse()
+        collision.impulse += delta
+        collision.impulse = max(0, collision.impulse)
+        delta = collision.impulse - old_impulse
+        collision.apply_impulse(delta * n)
 
+    def on_update(self, delta_time):
+        g = arcade.Vec2(0.0, -GRAVITY * delta_time)
+        for body in self.bodies:
+            if body.inv <= 0.0:
+                continue
+            body.velocity = body.velocity + g
+            body.velocity = body.velocity - body.velocity * body.velocity.length() * DRAG * delta_time
+
+        # warm starts
+        for collision in self.collisions.values():
+            collision.apply_impulse(collision.impulse * collision.normal)
+
+        for i in range(self.iterations):
+            for idx, body in enumerate(self.bodies):
+                for other in self.bodies[idx:]:
+                    if body == other:
+                        continue
+                    self.collision(body, other)
         
-        if self.circle_collider.overlaps(self.mouse):
-            arcade.draw_text('collides!', self.circle_collider.position.x, self.circle_collider.position.y, arcade.color.WHITE, anchor_x='center')
-
+        if self.grabbed is not None:
+            self.grabbed.velocity = arcade.Vec2()
         
-        if self.inverse_collider.overlaps(self.mouse):
-            arcade.draw_text('collides!', self.inverse_collider.position.x, self.inverse_collider.position.y, arcade.color.WHITE, anchor_x='center')
+        for body in self.bodies:
+            body.shape.position = body.shape.position + body.velocity * delta_time
 
     def on_key_press(self, symbol, modifiers):
-        match symbol:
-            case arcade.key.KEY_1:
-                self.mouse_t = 1
-                self.mouse = None
-            case arcade.key.KEY_2:
-                self.mouse_t = 2
-                self.mouse = None
-            case arcade.key.KEY_3:
-                self.mouse_t = 3
-                self.mouse = None
+        self.bodies.append(Body(CircleCollider(arcade.Vec2(self.center_x, self.center_y), 30), 30, arcade.Vec2.from_heading(1.62, 300.0)))
 
-    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
-        self.mouse_size = max(1, min(1000, self.mouse_size + scroll_y * 10))
-        self._make_mouse((x, y), self.mouse_size)
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.grabbed is not None:
+            return
+        
+        p = arcade.Vec2(x, y)
+        for obj in self.bodies[1:]:
+            if obj.shape.contains(p):
+                self.grabbed = obj
+                self.g_mass = obj.mass
+                obj.mass = 0.0
+                obj.inv = 0.0
+    
+    def on_mouse_release(self, x, y, button, modifiers):
+        if self.grabbed is not None:
+            self.grabbed.mass = self.g_mass
+            self.grabbed.inv = 0.0 if self.g_mass == 0.0 else 1.0 / self.g_mass
+            self.grabbed = None
 
-    def on_mouse_motion(self, x, y, dx, dy):
-        self._make_mouse((x, y), self.mouse_size)
+    def on_mouse_drag(self, x, y, dx, dy, _buttons, _modifiers):
+        if self.grabbed is None:
+            return
+        self.grabbed.shape.position = arcade.Vec2(x, y)
+        self.grabbed.velocity = arcade.Vec2(dx, dy) / self.window.delta_time
